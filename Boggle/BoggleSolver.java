b9746f3b68134c75effb1942186538f1f15e121d
Altynai <Altynai.me@gmail.com>
2013-12-18 21:40:14 +0800

    Add self made trie

--- Boggle/BoggleSolver.java
+++ Boggle/BoggleSolver.java
@@ -2,29 +2,61 @@ import java.util.TreeSet;
 
 public class BoggleSolver {
 
-	private TrieST<Integer> trie = null;
 	private int max_link_length = 0;
 	private int dx[] = { -1, -1, -1, 0, 1, 1, 1, 0 };
 	private int dy[] = { -1, 0, 1, 1, 1, 0, -1, -1 };
 	private boolean used[][] = null;
-	
+
+	private Node root = new Node();
+	private static final int R = 26; // 'A' - 'Z'
+	private static final int NULL_VAL = -1;
+
+	private static class Node {
+		private int val;
+		private Node[] next;
+		private boolean has_subtree;
+
+		public Node() {
+			val = NULL_VAL;
+			has_subtree = false;
+			next = new Node[R];
+			for (int i = 0; i < R; i++)
+				next[i] = null;
+		}
+	}
+
+	private void put(String word, int val) {
+		Node current_node = root;
+		for (int i = 0; i < word.length(); i++) {
+			int index = word.charAt(i) - 'A';
+			if (current_node.next[index] == null)
+				current_node.next[index] = new Node();
+			current_node.has_subtree = true;
+			current_node = current_node.next[index];
+		}
+		current_node.val = val;
+	}
+
+	private Node get(String word) {
+		Node current_node = root;
+		for (int i = 0; i < word.length(); i++) {
+			int index = word.charAt(i) - 'A';
+			if (current_node.next[index] == null)
+				return null;
+			current_node = current_node.next[index];
+		}
+		return current_node;
+	}
+
 	// Initializes the data structure using the given array of strings as the
 	// dictionary.
 	// (You can assume each word in the dictionary contains only the uppercase
 	// letters A through Z.)
 	public BoggleSolver(String[] dictionary) {
-		trie = new TrieST<Integer>();
 		for (String word : dictionary) {
-			word = word.toUpperCase();
-			for (int i = 1; i < word.length(); i++) {
-				String sub_word = formatQu2Q(word.substring(0, i));
-				Integer sub_word_score = trie.get(sub_word);
-				if (sub_word_score == null)
-					trie.put(sub_word, 0);
-			}
-			word = formatQu2Q(word);
+			word = formatQu2Q(word.toUpperCase());
 			int score = calculateScore(word);
-			trie.put(word, score);
+			put(word, score);
 			if (word.length() > max_link_length)
 				max_link_length = word.length();
 		}
@@ -41,7 +73,7 @@ public class BoggleSolver {
 			TreeSet<String> result_set) {
 		int M = board.rows(), N = board.cols();
 		used = new boolean[M][N];
-		
+
 		for (int i = 0; i < M; i++) {
 			for (int j = 0; j < N; j++) {
 				used[i][j] = true;
@@ -55,29 +87,39 @@ public class BoggleSolver {
 	private void dfs_collect(BoggleBoard board, TreeSet<String> result_set,
 			int i, int j, String current_word) {
 
+		/*
+		 * Optimization One:
+		 * If current_word.length() > max_link_length,
+		 * it means that any word longer than current_word will
+		 * never appears in the dictionary. So there's no
+		 * need to continue searching.
+		 */
 		if (current_word.length() > max_link_length)
 			return;
-		if (!trie.longestPrefixOf(current_word).equals(current_word))
+		Node deepest_node = get(current_word);
+		if (deepest_node == null)
 			return;
-		
-		Integer current_word_score = trie.get(current_word);
-		if (current_word_score != null && current_word_score > 0)
+		if (deepest_node.val > 0)
 			result_set.add(formatQ2Qu(current_word));
-		int M = board.rows(), N = board.cols();
+		/*
+		 * Optimization Two:
+		 * If deepest_node.has_subtree is false,
+		 * it means that any word with this prefix
+		 * never appears in the dictionary, and also,
+		 * there's no need to continue searching.
+		 */
+		if (!deepest_node.has_subtree)
+			return;
 
+		int M = board.rows(), N = board.cols();
 		for (int k = 0; k < dx.length; k++) {
 			int _i = i + dx[k];
 			int _j = j + dy[k];
 			if (!check2DPosition(_i, _j, M, N) || used[_i][_j])
 				continue;
-			/*
-			 * Optimization If we get trie.longestPrefixOf(new_word) !=
-			 * new_word, it means the sub-tree of that node is null, so there is
-			 * no need to continue searching.
-			 */
-			
 			used[_i][_j] = true;
-			dfs_collect(board, result_set, _i, _j, current_word + board.getLetter(_i, _j));
+			dfs_collect(board, result_set, _i, _j,
+					current_word + board.getLetter(_i, _j));
 			used[_i][_j] = false;
 		}
 	}
@@ -91,13 +133,12 @@ public class BoggleSolver {
 	// (You can assume the word contains only the uppercase letters A through
 	// Z.)
 	public int scoreOf(String word) {
-		if (trie == null)
+		word = formatQu2Q(word);
+		Node deepest_node = get(word);
+		if (deepest_node.val == NULL_VAL)
 			return 0;
-		else {
-			word = formatQu2Q(word);
-			Integer score = trie.get(word);
-			return (score == null ? 0 : score.intValue());
-		}
+		else
+			return deepest_node.val;
 	}
 
 	private String formatQu2Q(String word) {
@@ -107,7 +148,7 @@ public class BoggleSolver {
 	private String formatQ2Qu(String word) {
 		return word.replaceAll("Q", "QU");
 	}
-	
+
 	private int calculateScore(String word) {
 		int len = 0;
 		for (int i = 0; i < word.length(); i++) {
